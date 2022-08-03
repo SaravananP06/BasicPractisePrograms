d6892e08f0a7cc2e4228e88ecbdb6346508ed53a
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-03 17:41:36 +0530

    [Saravanan]: Added program to find strings equal or not

---
+++ src/Day1/BridgeLabs/StringComparison.java
@@ -0,0 +1,20 @@
+package Day1.BridgeLabs;
+
+/*
+ * This program will compare two strings
+ * If both strings are equal then it will give output as true otherwise false 
+ */
+
+public class StringComparison {
+
+	public static void main(String[] args) {
+		// Variables in string 
+		String s1 = "Saravanan";
+		String s2 = "Saravanan";
+		
+		System.out.println(s1.equals(s2));
+		
+
+	}
+
+}
