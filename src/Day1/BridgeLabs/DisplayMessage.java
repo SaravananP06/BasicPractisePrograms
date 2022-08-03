0e0b6ce2ae5bb66b361df780568d112d71515e81
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-03 17:14:02 +0530

    [Saravanan]: Added program to print some message

---
+++ src/Day1/BridgeLabs/DisplayMessage.java
@@ -0,0 +1,13 @@
+package Day1.BridgeLabs;
+/*
+ * This program will display the message
+ */
+public class DisplayMessage {
+
+	public static void main(String[] args) {
+		// printing some message
+		System.out.println("Hello World! Welcome, to Basic Practice Programs");
+
+	}
+
+}
