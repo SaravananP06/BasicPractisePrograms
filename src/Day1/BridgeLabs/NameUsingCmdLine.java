fc66c83d6b8256bc1774497285453568b9ba6ab9
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-04 19:30:05 +0530

    [Saravanan]: Added program to get users name from cmdline arguments

---
+++ src/Day1/BridgeLabs/NameUsingCmdLine.java
@@ -0,0 +1,15 @@
+package Day1.BridgeLabs;
+
+/* This program we will get user name from command line
+ * User will get name from command line arguments
+ */
+
+public class NameUsingCmdLine {
+
+	public static void main(String[] args) {
+		String name = args[3];
+        System.out.println("The name of the user is: " +name);
+
+	}
+
+}
