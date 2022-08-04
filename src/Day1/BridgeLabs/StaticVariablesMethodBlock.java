1a811ead858f1560dfb35db949169da7e8f9771b
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-04 20:03:52 +0530

    [Saravanan]: Added program to demonstrate Static Variables method and block

---
+++ src/Day1/BridgeLabs/StaticVariablesMethodBlock.java
@@ -0,0 +1,25 @@
+package Day1.BridgeLabs;
+/*
+ * This program demonstrates Static Variables, method and block  
+ */
+
+public class StaticVariablesMethodBlock {
+	static int x = 10;
+	   static int y;
+	   static void func(int z) {
+	      System.out.println("x = " + x);
+	      System.out.println("y = " + y);
+	      System.out.println("z = " + z);
+	   }
+	   static {
+		      System.out.println("Running static initialization block.");
+		      y = x + 5;
+		   }
+
+	public static void main(String[] args) {
+		func(2);
+		
+
+	}
+
+}
